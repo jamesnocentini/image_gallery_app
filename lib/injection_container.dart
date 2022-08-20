c85c43cd9adfab2fd691c8524f49eb80e8b9bc37
James Nocentini <james.nocentini@gmail.com>
2022-08-20 22:09:43 +0100

    fix dependency injection type inference

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
 import 'package:image_gallery_app/features/gallery/data/datasources/gallery_remote_data_source.dart';
 import 'package:image_gallery_app/features/gallery/data/repositories/gallery_repository_impl.dart';
+import 'package:image_gallery_app/features/gallery/domain/repositories/gallery_repository.dart';
 import 'package:image_gallery_app/features/gallery/presentation/bloc/gallery_bloc.dart';
 
 final sl = GetIt.instance;
@@ -10,7 +11,7 @@ void init() {
   // Bloc
   sl.registerFactory(() => GalleryBloc(galleryRepository: sl()));
   // Repository
-  sl.registerLazySingleton<GalleryRepositoryImpl>(
+  sl.registerLazySingleton<GalleryRepository>(
     () => GalleryRepositoryImpl(galleryRemoteDataSource: sl()),
   );
   // Data source
