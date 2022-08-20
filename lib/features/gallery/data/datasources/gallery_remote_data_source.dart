019c4f1721783cc865d856801bb5aa99d4557b5e
James Nocentini <james.nocentini@gmail.com>
2022-08-20 20:57:49 +0100

    implement getGalleryImages http call in the remove data source

--- lib/features/gallery/data/datasources/gallery_remote_data_source.dart
+++ lib/features/gallery/data/datasources/gallery_remote_data_source.dart
@@ -1,9 +1,25 @@
-import 'package:dartz/dartz.dart';
-import 'package:image_gallery_app/core/exceptions/server_exception.dart';
-import 'package:image_gallery_app/features/gallery/data/models/image_model.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+import 'package:image_gallery_app/core/exceptions/exceptions.dart';
+import 'package:image_gallery_app/features/gallery/data/models/gallery_model/gallery_model.dart';
+
+const _baseUrl = 'https://picsum.photos/v2';
 
 class GalleryRemoteDataSource {
-  Future<Either<ServerException, List<ImageModel>>> getGalleryImages() async {
-    return await Future.value(Right([ImageModel()]));
+  final Client httpClient;
+
+  GalleryRemoteDataSource(this.httpClient);
+
+  Future<GalleryModel> getGalleryImages() async {
+    final uri = Uri.parse('$_baseUrl/list');
+    final response = await httpClient.get(uri);
+    if (response.statusCode == 200) {
+      return GalleryModel.fromJson({'imageModels': jsonDecode(response.body)});
+    }
+    throw ServerException();
   }
 }
---
+++ test/features/gallery/data/datasources/gallery_remote_data_source_test.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:image_gallery_app/core/exceptions/exceptions.dart';
+import 'package:image_gallery_app/features/gallery/data/datasources/gallery_remote_data_source.dart';
+import 'package:image_gallery_app/features/gallery/data/models/gallery_model/gallery_model.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:http/http.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+class MockClient extends Mock implements Client {}
+
+main() {
+  late MockClient mockClient;
+  late GalleryRemoteDataSource sut;
+
+  setUp(() {
+    mockClient = MockClient();
+    sut = GalleryRemoteDataSource(mockClient);
+    registerFallbackValue(Uri());
+  });
+
+  group('getGalleryImages', () {
+    final tSuccessfulResponse = Response(fixture('gallery.json'), 200);
+    final tUnsuccessfulResponse = Response('', 400);
+
+    test(
+      'should return the deserialized gallery model when the call is successful',
+      () async {
+        // arrange
+        when(() => mockClient.get(any()))
+            .thenAnswer((_) async => tSuccessfulResponse);
+        // act
+        final result = await sut.getGalleryImages();
+        // assert
+        verify(
+            () => mockClient.get(Uri.parse('https://picsum.photos/v2/list')));
+        expect(
+          result,
+          GalleryModel.fromJson(
+            {'imageModels': jsonDecode(fixture('gallery.json'))},
+          ),
+        );
+      },
+    );
+
+    test(
+      'should throw a ServerException when the call is unsuccessful',
+      () async {
+        // arrange
+        when(() => mockClient.get(any()))
+            .thenAnswer((_) async => tUnsuccessfulResponse);
+        // act
+        final call = sut.getGalleryImages;
+        // assert
+        expect(() => call(), throwsA(isA<ServerException>()));
+        verify(
+            () => mockClient.get(Uri.parse('https://picsum.photos/v2/list')));
+      },
+    );
+  });
+}
